86ec321a5b0203abfe0e2e171cffeb7ce358c60c
velocityshy <106914751+velocityshy@users.noreply.github.com>
2022-06-07 11:22:17 -0500

    Add files via upload

---
+++ learning inputs and variables sort of.py
@@ -0,0 +1,11 @@
+price_house = 1000000
+is_good = False
+is_bad = True
+
+if is_good:
+    down_payment = price_house * 0.1
+
+elif is_bad:
+    down_payment = price_house * 0.2
+
+print(f"down payment is: {down_payment}")
